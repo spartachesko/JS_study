241c82180350e39786cdff114e475978abd27430
borisov_spartak <fichker@gmail.com>
2022-01-12 11:01:02 +0300

    Задание 1 - выполнено

--- 02_variables-and-numbers/task_1.js
+++ 02_variables-and-numbers/task_1.js
@@ -3,7 +3,36 @@
 // Для x1 = -5, y1 = 8, x2 = 10, y2 = 5 площадь равна 45;
 // Для x1 = 5, y1 = 8, x2 = 5, y2 = 5 площадь равна 0;
 // Для x1 = 8, y1 = 1, x2 = 5, y2 = 1 площадь равна 0.
-let x1 = 2;
-let y1 = 3;
-let x2 = 10;
-let y2 = 5;
+
+
+function AreaRectangle(x1,x2,y1,y2){
+    let result = (x2-x1)*(y2-y1)
+    return Math.abs(result)
+};
+
+// let x1 = 2;
+// let y1 = 3;
+// let x2 = 10;
+// let y2 = 5;
+
+// let x1 = 10;
+// let y1 = 5;
+// let x2 = 2;
+// let y2 = 3;
+
+// let x1 = -5;
+// let y1 = 8;
+// let x2 = 10;
+// let y2 = 5;
+
+// let x1 = 5;
+// let y1 = 8;
+// let x2 = 5;
+// let y2 = 5;
+
+let x1 = 8;
+let y1 = 1;
+let x2 = 5;
+let y2 = 1;
+
+console.log(AreaRectangle(x1,x2,y1,y2));
